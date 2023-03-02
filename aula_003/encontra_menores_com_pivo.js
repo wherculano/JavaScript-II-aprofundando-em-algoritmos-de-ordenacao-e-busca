794678b8aa129a02ceda161c1add7049c59921b8
wagner <wagner.santos@accurate.com.br>
2023-03-01 22:59:03 -0300

    Dividindo com pivo

--- aula_003/encontra_menores_com_pivo.js
+++ aula_003/encontra_menores_com_pivo.js
@@ -1,6 +1,8 @@
 const listaLivros = require("./array")
 
 function encontraMenores(pivo, array){
+    /** Encontra a quantidade de valores abaixo do valor do pivo
+     * e adiciona o pivo no indice desta quantidade, ordenando-o */
     let qtdMenores = 0
     for(let atual=0; atual < array.length; atual++){
         let produtoAtual = array[atual]
@@ -13,6 +15,7 @@ function encontraMenores(pivo, array){
 }
 
 function trocaLugar(array, de, para){
+    /** Efetua a troca de posicoes entre dois valores do array */
     let elemento1 = array[de]
     let elemento2 = array[para]
 
@@ -21,4 +24,18 @@ function trocaLugar(array, de, para){
  
 }
 
-console.log(encontraMenores(listaLivros[2], listaLivros))
+function divideNoPivo(array){
+    let pivo = array[Math.floor(array.length / 2)]
+    encontraMenores(pivo, array)
+    let valoresMenores = 0
+    for(let indiceAnalisado=0; indiceAnalisado < array.length; indiceAnalisado++){
+        let itemAtual = array[indiceAnalisado]
+        if(itemAtual.preco < pivo.preco && itemAtual != pivo){
+            trocaLugar(array, indiceAnalisado,valoresMenores)
+            valoresMenores++
+        }
+    }
+    
+    return array
+}
+console.log(divideNoPivo(listaLivros))
